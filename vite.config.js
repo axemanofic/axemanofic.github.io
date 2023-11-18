b788a0179ed64fb4ad7c400e80d94c4195241bdd
axeman <Roman Sotnikov>
2023-11-18 19:58:29 +0300

    build: add optimize image plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,86 @@
-import { defineConfig } from "vite";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { defineConfig } from 'vite'
+
+const DEFAULT_OPTIONS = {
+  test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+  exclude: undefined,
+  include: undefined,
+  includePublic: true,
+  logStats: true,
+  ansiColors: true,
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupNumericValues: false,
+            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+          },
+          cleanupIDs: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+      'sortAttrs',
+      {
+        name: 'addAttributesToSVGElement',
+        params: {
+          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+        },
+      },
+    ],
+  },
+  png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 100,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 100,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 100,
+  },
+  tiff: {
+    // https://sharp.pixelplumbing.com/api-output#tiff
+    quality: 100,
+  },
+  // gif does not support lossless compression
+  // https://sharp.pixelplumbing.com/api-output#gif
+  gif: {},
+  webp: {
+    // https://sharp.pixelplumbing.com/api-output#webp
+    lossless: true,
+  },
+  avif: {
+    // https://sharp.pixelplumbing.com/api-output#avif
+    lossless: true,
+  },
+  cache: false,
+  cacheLocation: undefined,
+}
 
 export default defineConfig({
   base: '/',
   server: {
     port: 3000,
-    open: "/index.html",
+    open: '/index.html',
   },
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
   },
   preview: {
-    port: 8000
+    port: 8000,
   },
-  root: "src",
-});
+  root: 'src',
+  plugins: [
+    ViteImageOptimizer({
+      ...DEFAULT_OPTIONS,
+    }),
+  ],
+})
